db154d9669b6c31bc16a966dd221391abb801781
Lawrence <lawrence.langen@gmail.com>
2023-02-02 09:03:21 +0100

    Homework Update

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,15 @@ public class App {
     
     public static void main(String[] args) {
      
+        Mitarbeiter mitarbeiter1=new Mitarbeiter("Jackson", "Michael", "Performer", 1971);
+        Mitarbeiter mitarbeiter2=new Mitarbeiter("Jackson", "Michael", "Performer", 1971);
+        Mitarbeiter mitarbeiter3=new Mitarbeiter("Jackson", "Michael", "Performer", 1971);
+        Mitarbeiter mitarbeiter4=new Mitarbeiter("Jackson", "Michael", "Performer", 1971);
+
+        output(mitarbeiter1.getProperties("#familyName"));
+        output(mitarbeiter1.getProperties("#firstName"));
+        output(mitarbeiter1.getProperties("#role"));
+        output(mitarbeiter1.getProperties("#yearOfEntry"));
     }
 
     private static void output(String outputStr) {
