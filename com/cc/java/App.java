8d26f30c06a105f0031bfd2681cc8fae0ab1ca0e
Lawrence <lawrence.langen@gmail.com>
2023-02-03 13:00:18 +0100

    change

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,31 +1,30 @@
 package com.cc.java;
 
 public class App {
+
+	public static void main(String[] args) {
+	
+	Mitarbeiter mitarbeiter1 = new Mitarbeiter("Mütze", "Maxine", "CTO", 2017);
+	Mitarbeiter mitarbeiter2 = new Mitarbeiter("Mützerich", "Max", "COO", 2018);
+	Mitarbeiter mitarbeiter3 = new Mitarbeiter("Martinelli", "Silvia", "CEO", 2020);
+	
+	pollMitarbeiter(mitarbeiter1);
+	pollMitarbeiter(mitarbeiter2);
+	pollMitarbeiter(mitarbeiter3);
+
+	}
+
+	private static void pollMitarbeiter(Mitarbeiter ma) {
+		output(ma.getInfo("#name"));
+		output(ma.getInfo("#position"));
+		output(ma.getInfo("#time"));
+		output("-------------------");
+	} 
+
+	private static void output(String outStr) {
+		System.out.println(outStr);
+	}
+
+};
     
-    public static void main(String[] args) {
-     
-        Mitarbeiter mitarbeiter1=new Mitarbeiter("Jackson", "Michael", "Performer", 1971);
-        Mitarbeiter mitarbeiter2=new Mitarbeiter("Hathaway", "Anna", "Actor", 1984);
-        Mitarbeiter mitarbeiter3=new Mitarbeiter("Schulz", "Olaf", "Chancelor", 2021);
-
-        pollMitarbeiter(mitarbeiter1);
-	    pollMitarbeiter(mitarbeiter2);
-	    pollMitarbeiter(mitarbeiter3);
-    }
-    private static void pollMitarbeiter(Mitarbeiter ma){
-        output(ma.getProperties("#familyName"));
-        output(ma.getProperties("#firstName"));
-        output(ma.getProperties("#role"));
-        output(ma.getProperties("#yearOfEntry"));
-        output("--------------------");
-
-    }
-
-    private static void output(String outputStr) {
-        System.out.println(outputStr);
-    }
-
-
-
-}
 
